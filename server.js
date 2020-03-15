e9f0c2f9db85d3f960f08c7b649ed9357c732ceb
Thomas Ehmann <tehmann@lequest.com>
2020-03-15 11:41:39 +0100

    Add route files with express router

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Connect to DB
@@ -9,6 +9,12 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Success, the API is running.");
 });
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
