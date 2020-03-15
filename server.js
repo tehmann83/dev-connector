745142fb8c08629652081f9ba37c8688ab19026f
Thomas Ehmann <tehmann@lequest.com>
2020-03-15 11:09:08 +0100

    Add mongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Connect to DB
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Success, the API is running.");
