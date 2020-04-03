15dbe2a2ae904fe4c48edacc2a0eadd446d51b5b
Thomas Ehmann <tehmann@lequest.com>
2020-04-03 18:04:52 +0200

    Add Alert Component & Action Call

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "../src/components/layout/Navbar";
 import Landing from "../src/components/layout/Landing";
 import Register from "../src/components/auth/Register";
 import Login from "../src/components/auth/Login";
+import Alert from "../src/components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -16,6 +17,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
 // import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +21,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords don't match");
+      setAlert("Passwords don't match", "danger");
     } else {
       console.log("SUCCESS");
       /* NOTE: Just to show what is possible here - axios import needed!
@@ -104,4 +107,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
