5fb45163a894a69d27da363a371892c6c6069273
Thomas Ehmann <tehmann@lequest.com>
2020-04-03 11:01:49 +0200

    Add posts routes

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const PostSchema = mongoose.Schema({
   user: {
-    type: Schema.type.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "users"
   },
   text: {
