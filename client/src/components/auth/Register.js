450872d7c75c578e1dfcaca6953e3bc630f9d9c4
Thomas Ehmann <tehmann@lequest.com>
2020-04-03 16:11:09 +0200

    Request example & Login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from "react";
+// import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +15,32 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords don't match");
     } else {
-      console.log(formData);
+      console.log("SUCCESS");
+      /* NOTE: Just to show what is possible here - axios import needed!
+       const newUser = {
+        name,
+        email,
+        password
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json"
+          }
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+
+        console.log(res.data);
+      } catch (err) {
+        console.log(err.response.data);
+      }*/
     }
   };
 
@@ -76,7 +98,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
