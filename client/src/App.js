cc34b846f877624bfc8480e4323faa5799c3a3cf
Thomas Ehmann <tehmann@lequest.com>
2020-04-07 23:01:50 +0200

    Add CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "../src/components/layout/Navbar";
-import Landing from "../src/components/layout/Landing";
-import Register from "../src/components/auth/Register";
-import Login from "../src/components/auth/Login";
-import Alert from "../src/components/layout/Alert";
-import Dashboard from "../src/components/dashboard/Dashboard";
-import PrivateRoute from "../src/components/routing/PrivateRoute";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -35,6 +36,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
